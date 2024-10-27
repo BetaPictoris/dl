5c379fca8a987fb63a0d4fe10e6156aea884af2c
beta <beta@hai.haus>
2024-10-26 18:18:58 -0700

    Here be dragoons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import SoftwareRow from "./components/SoftwareRow";
+import "./styles/app.css";
+import downloads from "./data/downloads.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <a href="#">Haihaus Downloads</a>
+
+        <nav>
+          <a href="#report">Report</a>
+          <a href="#request">Request</a>
+        </nav>
       </header>
-    </div>
+
+      <main>
+        <p>
+          Haihaus Downloads offers direct download links for software, it is not
+          affialated with Microsoft.
+        </p>
+
+        <table>
+          <thead>
+            <tr>
+              <td>Manufactuer</td>
+              <td>Software</td>
+              <td>Edition</td>
+              <td>Platform</td>
+              <td>Download</td>
+            </tr>
+          </thead>
+
+          <tbody>
+            {downloads.map((software) => (
+              <SoftwareRow data={software} />
+            ))}
+          </tbody>
+        </table>
+      </main>
+
+      <footer>
+        Created with ♥ by <a href="https://www.hai.haus">Daniel Hall</a>.
+      </footer>
+    </>
   );
 }
 
---
+++ src/components/SoftwareRow.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export default function SoftwareRow({ data }) {
+  const [edition, setEdition] = useState(0);
+  const [platform, setPlatform] = useState(0);
+
+  function changeEdition(payload) {
+    setEdition(document.getElementById("edition").value);
+    setPlatform(document.getElementById("platform").value);
+  }
+
+  function changePlatform(payload) {
+    setPlatform(document.getElementById("platform").value);
+  }
+
+  return (
+    <tr>
+      <td>{data.manufactuerName}</td>
+      <td>{data.softwareName}</td>
+      <td>
+        <select id="edition" onChange={changeEdition}>
+          {data.editions.map((edition) => (
+            <option value={data.editions.indexOf(edition)}>
+              {edition.name}
+            </option>
+          ))}
+        </select>
+      </td>
+      <td>
+        <select id="platform" onChange={changePlatform}>
+          {data.editions[edition].platforms.map((platform) => (
+            <option value={data.editions[edition].platforms.indexOf(platform)}>
+              {platform.name}
+            </option>
+          ))}
+        </select>
+      </td>
+      <td>
+        <button>Submit</button>
+      </td>
+    </tr>
+  );
+}
